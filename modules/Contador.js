217ff08d01210cd3b491f00c5f2c595f3ec0296b
Lucas Mofardini <lucasmofardini@gmail.com>
2022-07-12 21:26:52 -0300

    Start, stop and reset button

--- modules/Contador.js
+++ modules/Contador.js
@@ -1,6 +1,34 @@
-export function ativaContador(){
-    console.log('Contador');
+import { form } from '../script.js';
+
+
+let contador = 0;
+let jaEstaAtivo = false;
+var count;
+
+export function toggleContador(){
+    if(!jaEstaAtivo){
+        startContador();
+    }else{
+        pauseContador();
+    }
 }
+function startContador(){
+    jaEstaAtivo = true;
+    form.btnToggle.innerText = 'Stop';
 
-
-
+    count = setInterval((()=>{
+        contador++;
+        form.inputContador.value = contador;
+    }), 1000)
+}
+function pauseContador(){
+    form.btnToggle.innerText = 'Start';
+    jaEstaAtivo = false;
+    clearInterval(count);
+}
+export function resetContador(){
+    pauseContador()
+    contador = 0;
+    
+    form.inputContador.value = contador;
+}
